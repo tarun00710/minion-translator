fee2f2650e9e652896c70aa764a74a6e77f457f2
tarun00710 <tarunlal07@gmail.com>
2020-12-03 17:19:44 +0530

    doc-updated

---
+++ script.js
@@ -0,0 +1,26 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#outputDivison");
+
+
+var serverURL = "https://api.funtranslations.com/translate/fudd.json";
+
+function getTranslationURL(input){
+    return serverURL +"?"+"text"+ input;
+}
+
+function errorHandler(error){
+    console.log("error occured",error)
+}
+function clickHandler(){
+    var inputText=txtInput.value;
+    fetch(getTranslationURL(inputText))
+    .then(response=>response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+btnTranslate.addEventListener("click",clickHandler);
+
