6c1b252c87ce6615a2e57c87a4d991c3e8e1cdd1
tarun00710 <tarunlal07@gmail.com>
2020-12-03 17:29:30 +0530

    doc-updated scriptjs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-Translate");
+var txtInput = document.querySelector("#txt-Input");
 var outputDiv = document.querySelector("#outputDivison");
 
 
